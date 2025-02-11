1aca82e7d0cc6ca3508a142d533be3ef0cc9a00c
Márcio Forner <marcioforner03@gmail.com>
2025-02-11 20:52:26 -0300

    -> subindo arquivos
    -> subindo arquivos de projeto

---
+++ My project/Assets/SideWall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo) {
+        if (hitInfo.tag == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitInfo.gameObject.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+
+}
